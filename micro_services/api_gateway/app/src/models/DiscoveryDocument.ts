ddef3dce864dd3db50500ee6abece3352f945fc7
Mohammed Sitni <msitni1337@gmail.com>
2025-04-21 17:11:46 +0100

    Updated nginx conf and discovery document

--- micro_services/api_gateway/app/src/models/DiscoveryDocument.ts
+++ micro_services/api_gateway/app/src/models/DiscoveryDocument.ts
@@ -5,14 +5,14 @@ export const discoverDocument = {
         {
             description: 'Internal: Used for getting OAuth Exchange code back from google identity service, returns jwt cookie',
             notes: 'don\'t use the decoded jwt to infer user\'s info instead fetch the user info route.',
-            route: '/OAuth/code',
+            route: '/api/OAuth/code',
             QueryParams: [{ name: 'state' }, { name: 'code' }, { name: 'scope' }],
             method: 'GET'
         },
         OAuthStateRoute:
         {
             description: 'HTTP: Get a random state code to protect against CSRF attacks.',
-            route: '/OAuth/state',
+            route: '/api/OAuth/state',
             method: 'GET'
         }
     },
@@ -20,7 +20,7 @@ export const discoverDocument = {
         CheckUserDisplayNameAvailableRoute:
         {
             description: 'HTTP: check for display name availablity. returns 200 (available), 406 (not available), 400 (bad request)',
-            route: '/user/namecheck',
+            route: '/api/user/namecheck',
             QueryParams: [{ name: 'username', constraint: '>2 chars' }],
             method: 'GET'
         },
@@ -28,7 +28,7 @@ export const discoverDocument = {
         {
             description: 'HTTP: create a new user using a username and password. If no picture is provided a default one will be assigned. returns jwt cookie or error message',
             notes: 'don\'t use the decoded jwt to infer user\'s info instead fetch the user info route.',
-            route: '/user/signup',
+            route: '/api/user/signup',
             multipart_params: [{ name: 'username', type: 'text/plain', constraint: '>2 chars' }, { name: 'password', type: 'text/plain', constraint: '>7 chars' }, { name: 'picture', type: 'image/jpeg' }],
             method: 'POST'
         },
@@ -36,7 +36,7 @@ export const discoverDocument = {
         {
             description: 'HTTP: sign in user using username and password. returns jwt cookie or 401',
             notes: 'don\'t use the decoded jwt to infer user\'s info instead fetch the user info route.',
-            route: '/user/signin',
+            route: '/api/user/signin',
             multipart_params: [{ name: 'username', type: 'text/plain', constraint: '>2 chars' }, { name: 'password', type: 'text/plain', constraint: '>7 chars' }],
             method: 'POST'
         }
@@ -45,14 +45,14 @@ export const discoverDocument = {
         Enable2FA:
         {
             description: 'HTTP: Enable TOTP codes',
-            route: '/2FA/enable',
+            route: '/api/2FA/enable',
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'POST'
         },
         Disable2FA:
         {
             description: 'HTTP: Disable TOTP codes',
-            route: '/2FA/disable',
+            route: '/api/2FA/disable',
             multipart_params: [{ name: 'code', type: 'text/plain', constraint: '=6 digits' }],
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'POST'
@@ -60,7 +60,7 @@ export const discoverDocument = {
         VerifyCode:
         {
             description: 'HTTP: Verify TOTP code to complete sign in and get JWT token',
-            route: '/2FA/verify',
+            route: '/api/2FA/verify',
             QueryParams: [{ name: 'state' }],
             multipart_params: [{ name: 'code', type: 'text/plain', constraint: '=6 digits' }],
             method: 'POST'
@@ -70,7 +70,7 @@ export const discoverDocument = {
         FetchUserInfoRoute:
         {
             description: 'HTTP: Get information about the user with uid.',
-            route: '/user/info',
+            route: '/api/user/info',
             QueryParams: [{ name: 'uid' }],
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'GET'
@@ -78,7 +78,7 @@ export const discoverDocument = {
         UpdateUserInfoRoute:
         {
             description: 'HTTP: update user information by providing one or more params as field.',
-            route: '/user/info',
+            route: '/api/user/info',
             multipart_params: [{ name: 'username', type: 'text/plain', constraint: '>2 chars' }, { name: 'bio', type: 'text/plain' }, { name: 'picture', type: 'image/jpeg' }],
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'POST'
@@ -86,7 +86,7 @@ export const discoverDocument = {
         RemoveUserProfileRoute:
         {
             description: 'HTTP: remove user\'s profile picture.',
-            route: '/user/remove_picture',
+            route: '/api/user/remove_picture',
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'DELETE'
         }
@@ -95,21 +95,21 @@ export const discoverDocument = {
         ListFriendsRoute:
         {
             description: 'HTTP: Get current user friends list.',
-            route: '/friends',
+            route: '/api/friends',
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'GET'
         },
         ListFriendsRequestsRoute:
         {
             description: 'HTTP: Get current user friends requests.',
-            route: '/friends/requests',
+            route: '/api/friends/requests',
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'GET'
         },
         SendFriendRequestRoute:
         {
             description: 'HTTP: send a request to the user with uid specified in query param.',
-            route: '/friends/request',
+            route: '/api/friends/request',
             QueryParams: [{ name: 'uid' }],
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'POST'
@@ -117,7 +117,7 @@ export const discoverDocument = {
         AcceptFriendRequestRoute:
         {
             description: 'HTTP: accept a request specified by uid of the request in query param.',
-            route: '/friends/accept',
+            route: '/api/friends/accept',
             QueryParams: [{ name: 'uid' }],
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'POST'
@@ -125,7 +125,7 @@ export const discoverDocument = {
         DenyFriendRequestRoute:
         {
             description: 'HTTP: deny a request specified by uid of the request in query param.',
-            route: '/friends/deny',
+            route: '/api/friends/deny',
             QueryParams: [{ name: 'uid' }],
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'POST'
@@ -133,7 +133,7 @@ export const discoverDocument = {
         RemoveFriendRoute:
         {
             description: 'HTTP: remove friend specified by uid in query param.',
-            route: '/friends/remove',
+            route: '/api/friends/remove',
             QueryParams: [{ name: 'uid' }],
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'POST'
@@ -144,28 +144,28 @@ export const discoverDocument = {
         {
             description: 'WebSocket(wss): listen on notifications for ui update.',
             notes: 'due to limitations of ws, awkward protocol header is used to inject auth token, luckily this is easy to pass using vanilla JS WebSocket (see tests/websocket).',
-            route: '/notifications/push_notification',
+            route: '/api/notifications/push_notification',
             headers: [{ name: 'Sec-WebSocket-Protocol', value: '{{jwt_token}}' }],
             method: 'GET'
         },
         ListUnread:
         {
             description: 'HTTP: list unread notifications.',
-            route: '/notifications/list_unread',
+            route: '/api/notifications/list_unread',
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'GET'
         },
         ListAll:
         {
             description: 'HTTP: list all notifications.',
-            route: '/notifications/list_all',
+            route: '/api/notifications/list_all',
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'GET'
         },
         MarkAsRead:
         {
             description: 'HTTP: mark one or multiple notifications as read separated by \';\'.',
-            route: '/notifications/mark_as_read',
+            route: '/api/notifications/mark_as_read',
             QueryParams: [{ name: 'uids' }],
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'POST'
@@ -173,7 +173,7 @@ export const discoverDocument = {
         Delete:
         {
             description: 'HTTP: delete one or multiple notifications separated by \';\'.',
-            route: '/notifications/delete',
+            route: '/api/notifications/delete',
             QueryParams: [{ name: 'uids' }],
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'POST'
@@ -181,7 +181,7 @@ export const discoverDocument = {
         PokeFriend:
         {
             description: 'HTTP: send a poke notification to a friend.',
-            route: '/poke',
+            route: '/api/poke',
             QueryParams: [{ name: 'uid' }],
             headers: [{ name: 'Authorization', value: 'Bearer {{jwt_token}}' }],
             method: 'POST'
