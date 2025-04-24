30494187d3eeaa75285f6d50d114ddb1f37c296c
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2025-04-24 11:02:26 +0100

    refactor: update toast container styling and restructure main initialization logic

--- front_end/app/src/components/toast.ts
+++ front_end/app/src/components/toast.ts
@@ -35,7 +35,7 @@ class ToastContainer extends HTMLElement {
 
   connectedCallback() {
     this.className =
-      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]";
+      "fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px] space-y-2";
     window.addEventListener(TOAST_EVENT_NAME, this.handleEvent);
   }
 
--- front_end/app/src/main.ts
+++ front_end/app/src/main.ts
@@ -1,20 +1,19 @@
 import { getUser } from "./api/user";
 import "./style.css";
+import { handleEffect } from "./utils";
 import.meta.glob("./components/**/*.ts", { eager: true });
 import.meta.glob("./pages/**/*.ts", { eager: true });
 
-async function main() {
+document.addEventListener("DOMContentLoaded", () => {
   if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     document.documentElement.classList.add("dark");
   }
 
-  window._currentUser = await getUser();
-  const root = document.querySelector("#app");
-  if (!root) throw Error("App Root Not Found!");
+  handleEffect(document.body, async () => {
+    window._currentUser = await getUser();
+    const root = document.querySelector("#app");
+    if (!root) throw Error("App Root Not Found!");
 
-  root.replaceChildren(document.createElement("app-router"));
-}
-
-document.addEventListener("DOMContentLoaded", async () => {
-  await main();
+    root.replaceChildren(document.createElement("app-router"));
+  });
 });
