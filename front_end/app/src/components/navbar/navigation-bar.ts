2e5da522c0d5df6a8e29620f2fd980a4337e2fab
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2025-04-30 11:32:26 +0100

    feat: Add Home link to NavigationBar
    - Introduced a new "Home" link in the NavigationBar for easier navigation to the homepage.

--- front_end/app/src/components/navbar/navigation-bar.ts
+++ front_end/app/src/components/navbar/navigation-bar.ts
@@ -25,6 +25,10 @@ class NavigationBar extends HTMLElement {
         ]
       : [
           {
+            name: "Home",
+            href: "/",
+          },
+          {
             name: "LeaderBoard",
             href: "/leaderboard",
           },
