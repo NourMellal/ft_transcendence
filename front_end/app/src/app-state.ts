c3c4b0a554c9fb7d89ce73248f6ab7e34a9784e0
OUSSAMA BENCHKRON SAID <obenchkroune01@gmail.com>
2025-05-09 07:35:19 +0100

    Feat: Add notifications state to app state management

--- front_end/app/src/app-state.ts
+++ front_end/app/src/app-state.ts
@@ -1,9 +1,10 @@
 import { FriendRequest } from './api/friends';
-import { User } from './api/user';
+import { NotificationData, User } from './api/user';
 import { createStateStore } from './lib/state';
 
 const user = createStateStore<User | null>(null);
 const pushNotification = createStateStore<WebSocket | null>(null);
 const friendRequests = createStateStore<(FriendRequest & User)[] | null>(null);
+const notifications = createStateStore<NotificationData | null>(null);
 
-export { user, pushNotification, friendRequests };
+export { user, pushNotification, friendRequests, notifications };
