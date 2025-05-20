468ca1149c54831ed022e00bad2b8279a9ce1742
OUSSAMA BENCHKRON <obenchkroune01@gmail.com>
2025-05-20 15:34:02 +0100

    Refactor: update NotFound component layout

--- front_end/app/src/pages/not-found.ts
+++ front_end/app/src/pages/not-found.ts
@@ -3,7 +3,9 @@ import { html } from '~/lib/html';
 export default class NotFound extends HTMLElement {
   render() {
     this.replaceChildren(html`
-      <div class="flex flex-col items-center justify-center min-h-screen space-y-4">
+      <div
+        class="-mb-8 md:-mt-20 -mt-16 flex flex-col items-center justify-center min-h-screen space-y-4"
+      >
         <h1 class="text-4xl font-bold tracking-tight">404 Not Found</h1>
         <p class="text-muted-foreground text-center">The page you're looking for doesn't exist.</p>
         <a href="/" class="btn-primary"> Go back home </a>
