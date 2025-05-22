01fd9287fb232d7be1d2219627ad3858692f4e84
OUSSAMA BENCHKRON SAID <obenchkr@student.1337.ma>
2025-05-22 19:28:31 +0100

    Refactor game frontend code for improved readability and consistency
    - Updated import statements in main.ts for uniformity.
    - Reformatted element retrievals in main.ts for better readability.
    - Enhanced async function formatting in main.ts for clarity.
    - Adjusted CSS styles for consistency in styles.css, including color and layout adjustments.
    - Added responsive design improvements in styles.css for better mobile compatibility.
    - Enabled console and debugger drops in vite.config.ts for production builds.
    - Refactored VaultClient.ts for improved code structure and error handling, including recursive retry on failure.

--- front_end/app/game/src/game2v2.ts
+++ front_end/app/game/src/game2v2.ts
@@ -1,58 +1,68 @@
-import { gamesboard, winnerBoard, aiRematch , winnerText, resetMatch, sendWinOrLose, getNewMatch } from "./main.js";
-import { gameRunning, startGame , games , paddel3, paddel4} from "./game.js";
-import { getParticepantNames , tempName, player2Name, player1Name, reTournment} from "./tournment.js";
+import {
+  gamesboard,
+  winnerBoard,
+  aiRematch,
+  winnerText,
+  resetMatch,
+  sendWinOrLose,
+  getNewMatch,
+} from './main.js';
+import { gameRunning, startGame, games, paddel3, paddel4 } from './game.js';
+import {
+  getParticepantNames,
+  tempName,
+  player2Name,
+  player1Name,
+  reTournment,
+} from './tournment.js';
 
+export const reMatch2v2 = document.getElementById(
+  'reset2v2'
+) as HTMLButtonElement;
+let matchUID: string;
 
-export const reMatch2v2 = document.getElementById('reset2v2') as HTMLButtonElement;
-let matchUID:string;
-
-export async function    startGame2v2(){
-    try{
-
-        matchUID = await getNewMatch(2);
-        paddel3.style.display = 'flex';
-        paddel4.style.display = 'flex';
-        games.gametype = '2v2';
-        player2Name.innerText = tempName.p1;
-        gamesboard.style.display = 'none';
-        winnerBoard.style.display = 'none';
-        if (tempName.p2 === ''  || tempName.p3 === '' || tempName.p4 === '')
-            await getParticepantNames();
-        if (tempName.p2 != '' &&  tempName.p3 != '' && tempName.p4 != ''){ 
-            
-            player1Name.innerText = tempName.p1 + '\n\n' + tempName.p3;
-            player2Name.innerText = tempName.p2 + '\n\n' + tempName.p4;
-            document.addEventListener('keyup',  game2v2);
-        }
-    }catch(e:unknown)
-    {
-        if (e instanceof Error)
-        {
-            console.log(e.message);
-                // TODO:print error with toast
-        }
+export async function startGame2v2() {
+  try {
+    matchUID = await getNewMatch(2);
+    paddel3.style.display = 'flex';
+    paddel4.style.display = 'flex';
+    games.gametype = '2v2';
+    player2Name.innerText = tempName.p1;
+    gamesboard.style.display = 'none';
+    winnerBoard.style.display = 'none';
+    if (tempName.p2 === '' || tempName.p3 === '' || tempName.p4 === '')
+      await getParticepantNames();
+    if (tempName.p2 != '' && tempName.p3 != '' && tempName.p4 != '') {
+      player1Name.innerText = player1Name.innerText + '\n\n' + tempName.p3;
+      player2Name.innerText = tempName.p2 + '\n\n' + tempName.p4;
+      document.addEventListener('keyup', game2v2);
     }
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      console.log(e.message);
+      // TODO:print error with toast
+    }
+  }
 }
 
-async function    game2v2(event: KeyboardEvent){
-    let res :number = 0;
-    if (!gameRunning && player2Name.innerText)
-        res = await startGame(event);
-    if (res === 3){
-        winnerText.innerText = tempName.p1 + " and " + tempName.p3 + "\nwin";
-        await sendWinOrLose(matchUID, true);
-    }
-    else if (res === 2) {
-        winnerText.innerText = tempName.p2 + " and " + tempName.p4 + "\nwin";
-        await sendWinOrLose(matchUID, false);
-    }
-    if (res === 2 || res === 3){
-        res = 0;
-        reTournment.style.display = 'none';
-        aiRematch.style.display = 'none';
-        resetMatch.style.display = 'none';
-        reMatch2v2.style.display = 'block';
-        winnerBoard.style.display = 'flex';
-        document.removeEventListener('keyup', game2v2);
-    } 
+async function game2v2(event: KeyboardEvent) {
+  let res: number = 0;
+  if (!gameRunning && player2Name.innerText) res = await startGame(event);
+  if (res === 3) {
+    winnerText.innerText =
+      player1Name.innerText + ' and ' + tempName.p3 + '\nwin';
+    await sendWinOrLose(matchUID, true);
+  } else if (res === 2) {
+    winnerText.innerText = tempName.p2 + ' and ' + tempName.p4 + '\nwin';
+    await sendWinOrLose(matchUID, false);
+  }
+  if (res === 2 || res === 3) {
+    res = 0;
+    reTournment.style.display = 'none';
+    aiRematch.style.display = 'none';
+    resetMatch.style.display = 'none';
+    reMatch2v2.style.display = 'block';
+    winnerBoard.style.display = 'flex';
+    document.removeEventListener('keyup', game2v2);
+  }
 }
--- front_end/app/game/src/main.ts
+++ front_end/app/game/src/main.ts
@@ -1,19 +1,27 @@
-import { startTournment , reTournment, player1Name} from "./tournment.js";
-import { quit } from "./quit.js";
-import { game1v1 } from "./game1v1.js"
-import { startAiGame } from "./ai.js";
-import { startGame2v2 , reMatch2v2} from "./game2v2.js";
-import { resizeGame , paddel3, paddel4} from "./game.js";
+import { startTournment, reTournment, player1Name } from './tournment.js';
+import { quit } from './quit.js';
+import { game1v1 } from './game1v1.js';
+import { startAiGame } from './ai.js';
+import { startGame2v2, reMatch2v2 } from './game2v2.js';
+import { resizeGame, paddel3, paddel4 } from './game.js';
 
 export const gamesboard = document.getElementById('games') as HTMLDivElement;
-export const winnerBoard  = document.getElementById('frame2') as HTMLDivElement;
+export const winnerBoard = document.getElementById('frame2') as HTMLDivElement;
 export const button1v1 = document.getElementById('v1') as HTMLDivElement;
-export const resetMatch = document.getElementById('resetMatch') as HTMLButtonElement;
-export const winnerText = document.getElementById('winnerText') as HTMLHeadingElement;
+export const resetMatch = document.getElementById(
+  'resetMatch'
+) as HTMLButtonElement;
+export const winnerText = document.getElementById(
+  'winnerText'
+) as HTMLHeadingElement;
 export const username = document.getElementById('userName') as HTMLDivElement;
-export const aiButton = document.getElementById('AI') as HTMLButtonElement
-export const tournment = document.getElementById('tournment') as HTMLButtonElement;
-export const aiRematch = document.getElementById('resetMatchAi') as HTMLButtonElement;
+export const aiButton = document.getElementById('AI') as HTMLButtonElement;
+export const tournment = document.getElementById(
+  'tournment'
+) as HTMLButtonElement;
+export const aiRematch = document.getElementById(
+  'resetMatchAi'
+) as HTMLButtonElement;
 export const game2v2 = document.getElementById('V2') as HTMLButtonElement;
 // export let username_text : string = '';
 const quitGame = document.getElementById('quit') as HTMLButtonElement;
@@ -29,46 +37,46 @@ window.addEventListener('resize', resizeGame);
 
 gameMenu();
 
-
-export async  function getNewMatch(type:number) :Promise<string>{
-    let res = await fetch(
-        `https://transcendence.fr/api/match/create?match_type=${type}`, {method:"POST",body:new FormData()});
-        if (!res.ok){
-            throw Error(await res.text());
-        }
-        let uid = await res.text();
-        console.log(uid);
-        return (uid);
-}
-    
-export async function sendWinOrLose(uid:string, win:boolean) :Promise<void>{
-    let status = "https://transcendence.fr/api/match/win";
-    if (!win)
-        status = "https://transcendence.fr/api/match/lose";
-    let theForm = new FormData();
-    theForm.append("match_uid", uid)
-    try{
-        let res = await fetch(status, {method:"POST", body:theForm});
-        if (!res.ok)
-            throw Error(await res.text());
-    }catch(e){
-        console.log(e);
-    }
-    return;
+export async function getNewMatch(type: number): Promise<string> {
+  let res = await fetch(
+    `https://transcendence.fr/api/match/create?match_type=${type}`,
+    { method: 'POST', body: new FormData() }
+  );
+  if (!res.ok) {
+    throw Error(await res.text());
+  }
+  let uid = await res.text();
+  console.log(uid);
+  return uid;
 }
 
-
-export async function gameMenu()
-{
-    const UsernameQuery = await fetch("https://transcendence.fr/api/user/info?uid=me");
-    const UsernameQueryJson = await UsernameQuery.json() as { username: string };
-    player1Name.textContent = UsernameQueryJson.username;
-    paddel3.style.display = 'none';
-    paddel4.style.display = 'none';
-    gamesboard.style.display = 'flex';
-    button1v1.addEventListener('click', game1v1);
-    tournment.addEventListener('click', startTournment);
-    aiButton.addEventListener('click', startAiGame);
-    game2v2.addEventListener('click', startGame2v2);
+export async function sendWinOrLose(uid: string, win: boolean): Promise<void> {
+  let status = 'https://transcendence.fr/api/match/win';
+  if (!win) status = 'https://transcendence.fr/api/match/lose';
+  let theForm = new FormData();
+  theForm.append('match_uid', uid);
+  try {
+    let res = await fetch(status, { method: 'POST', body: theForm });
+    if (!res.ok) throw Error(await res.text());
+  } catch (e) {
+    console.log(e);
+  }
+  return;
 }
-  
+
+export async function gameMenu() {
+  const UsernameQuery = await fetch(
+    'https://transcendence.fr/api/user/info?uid=me'
+  );
+  const UsernameQueryJson = (await UsernameQuery.json()) as {
+    username: string;
+  };
+  player1Name.textContent = UsernameQueryJson.username;
+  paddel3.style.display = 'none';
+  paddel4.style.display = 'none';
+  gamesboard.style.display = 'flex';
+  button1v1.addEventListener('click', game1v1);
+  tournment.addEventListener('click', startTournment);
+  aiButton.addEventListener('click', startAiGame);
+  game2v2.addEventListener('click', startGame2v2);
+}
